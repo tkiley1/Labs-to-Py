2f94590b17e54b04292fe2b628fdd7274a80baa9
tkiley1 <Thomas_Kiley1@student.uml.edu>
2019-04-18 10:51:49 -0400

    Checkpoint 1 for lab 7

--- Lab7/main.py
+++ Lab7/main.py
@@ -10,18 +10,31 @@ def main():
 		sys.exit(1)
 	else:
 		print("All Unit Tests Passed")
-	head_string = String("Copy Me")
-	s_list = [None] * 100
-	for i in range(99):
-		c = String()
-		my_string_assignment(c, head_string)
-		s_list[i] = c
-	for i in s_list:
-		my_string_insertion(i)
-	
 
 
+	string_buckets = dict()
+	f = open("dictionary.txt", 'r')
+	tmp = String()
+	while(my_string_extraction(tmp, f)):
+		if tmp.get_size() not in string_buckets.keys():
+			string_buckets[tmp.get_size()] = [copy.copy(tmp)]
+		else:
+			string_buckets[tmp.get_size()] = string_buckets[tmp.get_size()] + [copy.copy(tmp)]
+		tmp = String()
 
+	d = raw_input("Enter a word length: ")
+	while(int(d) < 1 or int(d) > 21):
+		d = raw_input("Enter a word length between 1 and 21: ")
+	selected_bucket = string_buckets[int(d)]
+	while(1):
+		# Do Game
+			# get letter from user
+			# store in guessed already list
+			# sort the bucket into new buckets on index of the character
+				#need to account for multiple indexes as well
+			# grab the new bucket with the greatest length, and update the screen with the index key
+			# *whistle* AGAIN
+		return
 
 
 if __name__ == '__main__':
