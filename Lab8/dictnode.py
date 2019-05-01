8aef87f367b990fc1e2e5f8e19fa572430d8a463
tkiley1 <Thomas_Kiley1@student.uml.edu>
2019-05-01 16:21:30 -0400

    add comments to max heap implementation

--- Lab8/dictnode.py
+++ Lab8/dictnode.py
@@ -1,4 +1,9 @@
+
+# This is a simple dictionary node that we can use as nodes for our max heap.  Each node stores a key and a list of data
+# associated with that key.
 class dictnode:
+
+    # The initialization function instantiates our data, and our key.
     def __init__(self,key = None, data = None):
         self._key = key
         self._data = [data]
@@ -6,14 +11,21 @@ class dictnode:
             self._size = 1
         else:
             self._size = 0
+    # This function adds an entry to the list of data
     def add(self, data):
         if self._data == None:
             self._data = []
         self._data.append(data)
         self._size = self._size + 1
+
+    # This function returns the length of our data list
     def get_size(self):
         return self._size
+    
+    # This function returns the key associated with our data
     def get_key(self):
         return self._key
+    
+    # This function returns the list of our data
     def get_data(self):
         return self._data
--- Lab8/udict.py
+++ Lab8/udict.py
@@ -2,8 +2,12 @@ from dictnode import *
 import copy
 
 class udict:
+    # Initialization function - all we need is a list that holds our key value dictnodes
     def __init__(self):
         self._data = []
+
+    # This function maintains the max heap property within the data, so the largest bucket is always stored as the root
+    # node, or at index 0
     def balance(self):
         c = 1
         while c > 0:
@@ -14,6 +18,8 @@ class udict:
                     self._data[i] = copy.deepcopy(self._data[i+1])
                     self._data[i+1] = tmp
                     c = c + 1
+    # This function adds data to our heap.  If the key already exists, we add to the node and balance; if not, we create a new 
+    # node, add it to the tree, and balance.
     def add(self, key, string):
         for i in self._data:
             if i.get_key() == key:
@@ -23,16 +29,23 @@ class udict:
         tmp = dictnode(key, string)
         self._data.append(copy.deepcopy(tmp))
         self.balance()
+
+    # This function returns the list of string objects associated with a given key.
     def get_data(self, key):
         for i in self._data:
             if i.get_key() == key:
                 return i.get_data()
         return None
+
+    # This function returns all of the keys within our data
     def get_keys(self):
         keys = []
         for i in self._data:
             keys.append(i.get_key())
         return keys
+    
+    # This function returns the largest bucket within our data, which by default will be the root node of our 
+    # max-heap
     def get_largest(self):
         return self._data[0].get_data()
 
